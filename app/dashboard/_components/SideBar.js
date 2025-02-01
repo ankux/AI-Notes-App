e052e1964c35b19ae8bdbfb7463f491a9bf2e75d
Ankux <ankuxkr@gmail.com>
2025-02-01 19:16:38 +0530

    PDF Upload Interface

--- app/dashboard/_components/SideBar.js
+++ app/dashboard/_components/SideBar.js
@@ -3,6 +3,7 @@ import { Progress } from "@/components/ui/progress"
 import { Layout, Shield } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
+import UploadPdfDialog from './UploadPdfDialog'
 
 const SideBar = () => {
   return (
@@ -10,7 +11,10 @@ const SideBar = () => {
         <Image src={'/logo.svg'} alt='logo' width={170} height={120} />
 
         <div className='mt-10'>
-            <Button className='w-full'>Upload PDF</Button>
+
+            <UploadPdfDialog>
+                <Button className='w-full'>+ Upload PDF</Button>
+            </UploadPdfDialog>
 
             <div className='flex gap-2 items-center p-3 mt-5 hover:bg-slate-100 rounded-lg cursor-pointer'>
                 <Layout/>
